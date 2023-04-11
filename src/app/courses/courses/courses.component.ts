758d72e8749443bcea2555e7571aeb829137faba
uzielmenezes <uzielcivil@gmail.coom>
2023-04-11 07:46:01 -0300

    feat: courses table from angular material

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Course } from './model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end' }
+  ];
 
+  displayedColumns = ['name', 'category'];
+
+  constructor() { }
 }
