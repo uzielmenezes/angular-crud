722072d86a66e9628c4dadca7202a1ee958c576c
Uziel Menezes <uzielcivil@gmail.com>
2023-07-16 10:02:32 -0300

    feat: api endpoint

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -9,7 +9,7 @@ import { take, tap, delay } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(
     private httpClient: HttpClient
