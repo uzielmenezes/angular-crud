709a80ed1958f9ba5895e15dacb476a99558de8e
Uziel Menezes <uzielcivil@gmail.com>
2023-07-19 19:07:04 -0300

    feat: course form

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+  courseForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.courseForm = this.fb.group({
+      name: [null],
+      category: [null]
+    })
+  }
+
+  ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+
+  }
 }
