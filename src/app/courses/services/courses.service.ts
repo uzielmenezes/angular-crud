c24600f2441ebf007f6c96525f660945c5a069c3
Uziel Menezes <uzielcivil@gmail.com>
2023-07-22 09:33:41 -0300

    feat: save method for courses

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, take, tap } from 'rxjs';
 
 import { Course } from '../courses/model/course';
-import { take, tap, delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +22,9 @@ export class CoursesService {
         delay(500),
         tap(courses => console.log(courses))
       );
+  }
+
+  save(record: Course) {
+  return this.httpClient.post<Course>(this.API, record).pipe(take(1));
   }
 }
