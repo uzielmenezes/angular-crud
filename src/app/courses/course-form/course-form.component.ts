8f81b164db83bc0882f9bd02a9c578a326a70a73
Uziel Menezes <uzielcivil@gmail.com>
2023-07-22 11:07:38 -0300

    feat: location back logic + on success snackbar

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,20 +13,37 @@ import { CoursesService } from '../services/courses.service';
 export class CourseFormComponent implements OnInit {
   courseForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private coursesService: CoursesService, private snackBar: MatSnackBar){
+  constructor(
+    private fb: FormBuilder,
+    private coursesService: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {
     this.courseForm = this.fb.group({
       name: [null],
-      category: [null]
-    })
+      category: [null],
+    });
   }
 
-  ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
+  ngOnInit(): void {
+    /* TODO document why this method 'ngOnInit' is empty */
+  }
 
   onSubmit() {
     this.coursesService.save(this.courseForm.value).subscribe({
-      next: (result) => console.log(result),
+      next: (result) => {
+        console.log(result);
+        this.onSuccess();
+      },
       error: (_) => this.onError(),
     });
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso.', '', {
+      duration: 3000,
+    });
+    this.onCancel();
   }
 
   private onError() {
@@ -35,6 +53,6 @@ export class CourseFormComponent implements OnInit {
   }
 
   onCancel() {
-
+    this.location.back();
   }
 }
