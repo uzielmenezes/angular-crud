a28d1af71f92efe50fc890445e39bc7bdbfc9804
Uziel Menezes <uzielcivil@gmail.com>
2023-07-22 11:06:58 -0300

    fix: variable change

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -5,26 +5,22 @@ import { delay, take, tap } from 'rxjs';
 import { Course } from '../courses/model/course';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = 'api/courses';
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
-      .pipe(
-        take(1),
-        delay(500),
-        tap(courses => console.log(courses))
-      );
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      take(1),
+      delay(500),
+      tap((courses) => console.log(courses))
+    );
   }
 
-  save(record: Course) {
-  return this.httpClient.post<Course>(this.API, record).pipe(take(1));
+  save(course: Course) {
+    return this.httpClient.post<Course>(this.API, course).pipe(take(1));
   }
 }
