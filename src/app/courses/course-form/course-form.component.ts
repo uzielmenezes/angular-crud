0a5e24466f2c5e12f2a827b628338b8065e631a8
Uziel Menezes <uzielcivil@gmail.com>
2023-07-22 09:34:07 -0300

    feat: calling the service + error snackbar

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -9,7 +12,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CourseFormComponent implements OnInit {
   courseForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private coursesService: CoursesService, private snackBar: MatSnackBar){
     this.courseForm = this.fb.group({
       name: [null],
       category: [null]
@@ -19,7 +22,16 @@ export class CourseFormComponent implements OnInit {
   ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
 
   onSubmit() {
+    this.coursesService.save(this.courseForm.value).subscribe({
+      next: (result) => console.log(result),
+      error: (_) => this.onError(),
+    });
+  }
 
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', {
+      duration: 3000,
+    });
   }
 
   onCancel() {
