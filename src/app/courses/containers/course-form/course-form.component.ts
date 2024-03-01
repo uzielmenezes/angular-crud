05504b19bc774dc9f935e6667b897a213c05c793
Uziel <uzielcivil@gmail.com>
2024-03-01 12:16:22 -0300

    feat: creating formGroup for lessons + type adjusts

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,11 +1,36 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
 import { Course } from '../../model/course';
+import { Lesson } from '../../model/lesson';
 import { CoursesService } from '../../services/courses.service';
+
+type CourseForm = {
+  id: FormControl<string>;
+  name: FormControl<string>;
+  category: FormControl<string>;
+};
+
+type LessonForm = {
+  id: FormControl<string>;
+  name: FormControl<string>;
+  youtubeUrl: FormControl<string>;
+};
+
+enum ErrorMessage {
+  Required = 'Campo obrigatório!',
+  MinLength = 'Mínimo de 3 caracteres!',
+  MaxLength = 'Máximo de 50 caracteres!',
+  Default = 'Campo inválido!',
+}
 
 @Component({
   selector: 'app-course-form',
@@ -13,7 +38,7 @@ import { CoursesService } from '../../services/courses.service';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-  courseForm!: FormGroup;
+  courseForm!: FormGroup<CourseForm>;
 
   course!: Course;
 
@@ -49,7 +74,30 @@ export class CourseFormComponent implements OnInit {
     this.course = this.route.snapshot.data['course'];
   }
 
-  public onSubmit() {
+  private getLessons(course: Course): FormGroup<LessonForm>[] {
+    const lessons = [];
+
+    if (course?.lessons) {
+      course.lessons.forEach((lesson) =>
+        lessons.push(this.createLesoon(lesson))
+      );
+    } else {
+      lessons.push(this.createLesoon());
+    }
+    return lessons;
+  }
+
+  private createLesoon(
+    lesson: Lesson = { id: '', name: '', youtubeUrl: '' }
+  ): FormGroup<LessonForm> {
+    return this.fb.group({
+      id: [lesson.id],
+      name: [lesson.name],
+      youtubeUrl: [lesson.youtubeUrl],
+    });
+  }
+
+  public onSubmit(): void {
     if (this.courseForm.invalid) {
       this.courseForm.get('name')?.markAllAsTouched();
       this.courseForm.get('category')?.markAllAsTouched();
@@ -58,7 +106,7 @@ export class CourseFormComponent implements OnInit {
     }
   }
 
-  private saveCourse() {
+  private saveCourse(): void {
     this.coursesService.save(this.courseForm.value).subscribe({
       next: () => {
         this.onSuccess();
@@ -67,38 +115,38 @@ export class CourseFormComponent implements OnInit {
     });
   }
 
-  private onSuccess() {
+  private onSuccess(): void {
     this.snackBar.open('Curso salvo com sucesso.', '', {
       duration: 3000,
     });
     this.onCancel();
   }
 
-  private onError() {
+  private onError(): void {
     this.snackBar.open('Erro ao salvar curso.', '', {
       duration: 3000,
     });
   }
 
-  public onCancel() {
+  public onCancel(): void {
     this.location.back();
   }
 
-  public getErrorMessage(fieldName: string) {
+  public getErrorMessage(fieldName: string): ErrorMessage {
     const field = this.courseForm.get(fieldName);
 
     if (field?.hasError('required')) {
-      return 'Campo obrigatório!';
+      return ErrorMessage.Required;
     }
 
     if (field?.hasError('minlength')) {
-      return 'Mínimo de 3 caracteres!';
+      return ErrorMessage.MinLength;
     }
 
     if (field?.hasError('maxlength')) {
-      return 'Máximo de 50 caracteres!';
+      return ErrorMessage.MaxLength;
     }
 
-    return 'Campo inválido!';
+    return ErrorMessage.Default;
   }
 }
