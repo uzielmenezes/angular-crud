20c12b84b70e1c02c9fa06a21cb72c60dbae3e75
Uziel <uzielcivil@gmail.com>
2024-03-01 12:45:39 -0300

    chore: return types + enum

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -9,6 +9,13 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
 
+enum MessageToShow {
+  OnDeleteSuccess = 'Curso removido com sucesso!',
+  OnDeleteError = 'Erro ao tentar remover curso!',
+  OnCoursesFetchError = 'Erro ao carregar cursos.',
+  OnDeleteConfirm = 'Tem certeza que deseja remover esse curso?',
+}
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -29,34 +36,34 @@ export class CoursesComponent {
     this.refresh();
   }
 
-  private refresh() {
+  private refresh(): void {
     this.courses$ = this.coursesService.list().pipe(
       catchError((error) => {
-        this.onError('Erro ao carregar cursos.');
+        this.onError(MessageToShow.OnCoursesFetchError);
         return of([]);
       })
     );
   }
 
-  onError(errorMsg: String) {
+  onError(errorMsg: string): void {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg,
     });
   }
 
-  onAdd() {
+  onAdd(): void {
     this.router.navigate(['new'], { relativeTo: this.activatedRoute });
   }
 
-  onEdit(course: Course) {
+  onEdit(course: Course): void {
     this.router.navigate(['edit', course.id], {
       relativeTo: this.activatedRoute,
     });
   }
 
-  onDelete(course: Course) {
+  onDelete(course: Course): void {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      data: 'Tem certeza que deseja remover esse curso?',
+      data: MessageToShow.OnDeleteConfirm,
     });
 
     dialogRef.afterClosed().subscribe((result: boolean) => {
@@ -64,18 +71,22 @@ export class CoursesComponent {
         this.coursesService.remove(course.id).subscribe({
           next: () => {
             this.refresh();
-            this.snackBar.open('Curso removido com sucesso!', 'X', {
-              duration: 5000,
-              verticalPosition: 'top',
-              horizontalPosition: 'center',
-            });
+            this.onSuccess(MessageToShow.OnDeleteSuccess);
           },
           error: (err) => {
-            this.onError('Erro ao tentar remover curso!');
+            this.onError(MessageToShow.OnDeleteError);
             console.error(err);
           },
         });
       }
+    });
+  }
+
+  private onSuccess(successMsg: string) {
+    this.snackBar.open(successMsg, 'X', {
+      duration: 5000,
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
     });
   }
 }
