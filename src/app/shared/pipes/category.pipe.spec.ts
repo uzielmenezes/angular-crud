dd611e49b895bc8c97b9c2a1258bcbdd87fbfe5d
Uziel <uzielcivil@gmail.com>
2024-04-22 08:18:22 -0300

    feat: unit test for cateogry pipe

--- src/app/shared/pipes/category.pipe.spec.ts
+++ src/app/shared/pipes/category.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { CategoryPipe } from './category.pipe';
 
 describe('CategoryPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CategoryPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: CategoryPipe;
+
+  beforeEach(() => {
+    pipe = new CategoryPipe();
+  });
+
+  it('should transform "front-end" to "code"', () => {
+    const result = pipe.transform('front-end');
+    expect(result).toBe('code');
+  });
+
+  it('should transform "back-end" to "computer"', () => {
+    const result = pipe.transform('back-end');
+    expect(result).toBe('computer');
+  });
+
+  it('should transform any other value to "code"', () => {
+    const result = pipe.transform('other');
+    expect(result).toBe('code');
   });
 });
